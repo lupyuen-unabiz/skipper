ed5c72834ea3abca0ca35c570cf6c2787691202a
lupyuen-unabiz <ly.lee@unabiz.com>
2018-04-18 21:32:03 +0800

    Added go-get.cmd

---
+++ cmd/skipper/scripts/go-get.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+go get -v github.com/abbot/go-http-auth
+go get -v github.com/cjoudrey/gluahttp
+go get -v github.com/cjoudrey/gluaurl
+go get -v github.com/dimfeld/httppath
+go get -v github.com/oklog/ulid
+go get -v github.com/prometheus/client_golang/prometheus
+go get -v github.com/prometheus/client_golang/prometheus/promhttp
+go get -v github.com/rcrowley/go-metrics
+go get -v github.com/sirupsen/logrus
+go get -v github.com/sony/gobreaker
+go get -v github.com/szuecs/rate-limit-buffer
